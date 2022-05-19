a0f4b7597d1eefa3752f7f80b7224ab5c860f8b9
Denperidge <denperidge@gmail.com>
2022-05-19 17:29:22 +0200

    First succesful mask!

--- index.py
+++ index.py
@@ -1,18 +1,20 @@
 from glob import glob
 from io import BytesIO
-from PIL import Image
+from PIL import Image, ImageDraw
 from os import path
 #from svglib.svglib import svg2rlg
 from cairosvg import svg2png
 
 def svg_shape_import(path):
     with open(path, "r") as svg:
-        png
-        shape = Image.open(BytesIO(svg2png(svg.read()))) # https://stackoverflow.com/a/62345450
+        shape = svg.read()
 
     return shape
 
-heart_shape = svg_shape_import("vendor/heart.svg")
+def svg_to_image(data, width, height):
+    return Image.open(BytesIO(svg2png(data, output_width=width, output_height=height))) # https://stackoverflow.com/a/62345450
+
+heart_svg = svg_shape_import("vendor/heart.svg")
 
 new_filename = "export/{0}_{1}.png"
 
@@ -21,10 +23,22 @@ for flag_path in flags:
     flag_name = path.basename(flag_path)
     flag = Image.open(flag_path)
 
-    heart_flag = Image.composite(flag, flag, heart_shape)
+    transparant = Image.new("RGBA", (flag.width, flag.height), (255, 255, 255, 0))
 
 
-    heart_flag.save(new_filename.format(flag_name, "heart"))
+    heart_shape = transparant.copy()
+    heart_shape.paste(svg_to_image(heart_svg, transparant.width, transparant.height))
+
+    transparant.paste(flag, mask=heart_shape)
+
+
+
+    transparant.save("test.png")
+
+    #heart_flag = Image.composite(transparant, flag, heart_svg)
+
+
+    #heart_flag.save(new_filename.format(flag_name, "heart"))
     
 
 
