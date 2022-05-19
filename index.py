361b0e250170e1ca9aba1098a38c572edd7ec3b8
Denperidge <denperidge@gmail.com>
2022-05-19 17:09:09 +0200

    WIP Pillow implementation

--- index.py
+++ index.py
@@ -1,18 +1,30 @@
-from requests import request
-from json import loads
+from glob import glob
+from io import BytesIO
+from PIL import Image
+from os import path
+#from svglib.svglib import svg2rlg
+from cairosvg import svg2png
 
-def get(action, **additional_params):
-    preset_parameters = {
-        "format": "json",
-        "action": action
-        }
-    full_parameters = preset_parameters | additional_params
+def svg_shape_import(path):
+    with open(path, "r") as svg:
+        png
+        shape = Image.open(BytesIO(svg2png(svg.read()))) # https://stackoverflow.com/a/62345450
+
+    return shape
+
+heart_shape = svg_shape_import("vendor/heart.svg")
+
+new_filename = "export/{0}_{1}.png"
+
+flags = glob("vendor/Zaimki/static/flags/[!_]*")
+for flag_path in flags:
+    flag_name = path.basename(flag_path)
+    flag = Image.open(flag_path)
+
+    heart_flag = Image.composite(flag, flag, heart_shape)
+
+
+    heart_flag.save(new_filename.format(flag_name, "heart"))
     
-    return loads(request("GET", "https://lgbtqia.fandom.com/api.php", params=full_parameters).text)
 
 
-flag_query = get("query", list="allimages", titles=["Flags"])
-flags = flag_query["query"]["allimages"]
-print(len(flags))
-for flag in flags:
-    print(flag)
