7fdbcafc3a1f4906c36ee16cdccb609b047f9465
Denperidge <denperidge@gmail.com>
2022-05-19 17:45:06 +0200

    Modified svg & first succesful full run!

--- index.py
+++ index.py
@@ -18,27 +18,21 @@ heart_svg = svg_shape_import("vendor/heart.svg")
 
 new_filename = "export/{0}_{1}.png"
 
+#def flag_from_svg(mask_svg, original_flag, transparent_canvas):
+    
 flags = glob("vendor/Zaimki/static/flags/[!_]*")
 for flag_path in flags:
     flag_name = path.basename(flag_path)
     flag = Image.open(flag_path)
 
-    transparant = Image.new("RGBA", (flag.width, flag.height), (255, 255, 255, 0))
+    # Create a transparent canvas equal to the size of the flag for the masks to base themselves on
+    transparent = Image.new("RGBA", (flag.width, flag.height), (255, 255, 255, 0))
 
+    # Copy that transparenty canvas and paste the heart svg into it, at the correct size & height
+    heart_shape = transparent.copy()
+    heart_shape.paste(svg_to_image(heart_svg, transparent.width, transparent.height))
 
-    heart_shape = transparant.copy()
-    heart_shape.paste(svg_to_image(heart_svg, transparant.width, transparant.height))
-
-    transparant.paste(flag, mask=heart_shape)
-
-
-
-    transparant.save("test.png")
-
-    #heart_flag = Image.composite(transparant, flag, heart_svg)
-
-
-    #heart_flag.save(new_filename.format(flag_name, "heart"))
+    heart_flag = transparent.copy()
+    heart_flag.paste(flag, mask=heart_shape)
+    heart_flag.save(new_filename.format(flag_name, "heart"))
     
-
-
