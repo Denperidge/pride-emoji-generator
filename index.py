409515410029daec2aacd3b791760e555d938d96
Denperidge <denperidge@gmail.com>
2022-05-19 18:12:36 +0200

    Improved functions, added shape discovery

--- index.py
+++ index.py
@@ -5,20 +5,25 @@ from os import path
 #from svglib.svglib import svg2rlg
 from cairosvg import svg2png
 
-def svg_shape_import(path):
-    with open(path, "r") as svg:
-        shape = svg.read()
-
-    return shape
 
 def svg_to_image(data, width, height):
     return Image.open(BytesIO(svg2png(data, output_width=width, output_height=height))) # https://stackoverflow.com/a/62345450
 
-heart_svg = svg_shape_import("vendor/heart.svg")
+def filename_no_ext(original_path):
+    return path.splitext(path.basename(original_path))[0]
 
-new_filename = "export/{0}_{1}.png"
+# Import all svgs
+shape_paths = glob("shapes/*.svg")
+shapes = dict()
+for shape_path in shape_paths:
+    shape_name = filename_no_ext(shape_path)
 
-def flag_from_svg(transparent_canvas, original_flag, mask_svg):
+    with open(shape_path, "r") as shape_svg_data:
+        shapes[shape_name] = shape_svg_data.read()
+    
+
+
+def flag_from_svg(transparent_canvas, original_flag, mask_svg, mask_name):
     # Copy that transparenty canvas and paste the svg into it, at the correct size & height
     shape = transparent_canvas.copy()
     shape.paste(svg_to_image(mask_svg, transparent_canvas.width, transparent_canvas.height))
@@ -26,20 +31,21 @@ def flag_from_svg(transparent_canvas, original_flag, mask_svg):
     # Create a blank image the size of the flag, and paste the flag into it, but masked
     shape_flag = transparent_canvas.copy()
     shape_flag.paste(original_flag, mask=shape)
-    return shape_flag
+
+    shape_flag.save("export/{0}_{1}.png".format(flag_name, mask_name))
 
 # This glob excludes flags starting with _ & -, but this can be easily modified by modifying the glob!
 flags = glob("vendor/Zaimki/static/flags/[!_,-]*")
 
 for flag_path in flags:
-    flag_name = path.splitext(path.basename(flag_path))[0]
+    flag_name = filename_no_ext(flag_path)
     flag = Image.open(flag_path)
 
     # Create a transparent canvas equal to the size of the flag for the masks to base themselves on
     # This isn't done in the function to save processing power!
     transparent = Image.new("RGBA", (flag.width, flag.height), (255, 255, 255, 0))
 
-    
-    heart_flag = flag_from_svg(transparent, flag, heart_svg)
-    heart_flag.save(new_filename.format(flag_name, "heart"))
+    for shape_name in shapes:
+        flag_from_svg(transparent, flag, shapes[shape_name], shape_name)
+
     
